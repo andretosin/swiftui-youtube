35639b8c8148ba365d68d82086b30c0b1c8a6b2f
André Luiz Tosin <andre.tosin@portoseguro.com.br>
2024-05-18 19:29:25 -0300

    Initial Commit

---
+++ Youtube/YoutubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeApp.swift
+//  Youtube
+//
+//  Created by André Tosin on 18/05/24.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
